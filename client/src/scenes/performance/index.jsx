137187cfca2eeb7faf65783818f464f0744e29f7
Rupesh <shrestharupesh110@gmail.com>
2023-09-06 03:12:36 +1000

    minor comment changes

--- client/src/scenes/performance/index.jsx
+++ client/src/scenes/performance/index.jsx
@@ -2,45 +2,45 @@ import React from "react";
 import { Box, useTheme } from "@mui/material";
 import { useGetUserPerformanceQuery } from "state/api";
 import { useSelector } from "react-redux";
-//import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import Header from "components/Header";
-//import CustomColumnMenu from "components/DataGridCustomColumnMenu";
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
 
 const Performance = () => {
   const theme = useTheme();
   const userId = useSelector((state) => state.global.userId);
-  //const { data, isLoading } = useGetUserPerformanceQuery(userId);
+  const { data, isLoading } = useGetUserPerformanceQuery(userId);
 
-  // const columns = [
-  //   {
-  //     field: "_id",
-  //     headerName: "ID",
-  //     flex: 1,
-  //   },
-  //   {
-  //     field: "userId",
-  //     headerName: "User ID",
-  //     flex: 1,
-  //   },
-  //   {
-  //     field: "createdAt",
-  //     headerName: "CreatedAt",
-  //     flex: 1,
-  //   },
-  //   {
-  //     field: "products",
-  //     headerName: "# of Products",
-  //     flex: 0.5,
-  //     sortable: false,
-  //     renderCell: (params) => params.value.length,
-  //   },
-  //   {
-  //     field: "cost",
-  //     headerName: "Cost",
-  //     flex: 1,
-  //     renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
-  //   },
-  // ];
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "CreatedAt",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
 
   return (
     <Box m="1.5rem 2.5rem">
@@ -77,7 +77,7 @@ const Performance = () => {
         }}
       >
         <Header title="Please note that this page is still a work in progress."/>
-        {/* <DataGrid
+        <DataGrid
           loading={isLoading || !data}
           getRowId={(row) => row._id}
           rows={(data && data.sales) || []}
@@ -85,7 +85,7 @@ const Performance = () => {
           components={{
             ColumnMenu: CustomColumnMenu,
           }}
-        /> */}
+        />
       </Box>
     </Box>
   );
