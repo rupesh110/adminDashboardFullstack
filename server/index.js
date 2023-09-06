f7d2c58e0e1dd937f77f8073ced510dc823b5fab
Rupesh <shrestharupesh110@gmail.com>
2023-09-06 10:00:05 +1000

    sever reload problem

--- server/index.js
+++ server/index.js
@@ -10,6 +10,9 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 import OverallStat from './models/OverallStat.js';
+import { notFound, errorHandler } from './middleware/errorMidddleware.js';
+import connectDB from './config/db.js';
+import path from 'path'; // Import the path module
 
 //data imports
 import User from './models/User.js';
@@ -19,18 +22,19 @@ import Transaction from './models/Transaction.js';
 import AffiliateStat  from './models/AffiliateStat.js';
 import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat  } from './data/index.js';
 
-
-
 /* CONFIGURATION */
 dotenv.config();
+connectDB();
 const app = express();
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 
 /* ROUTES */
 app.use("/client", clientRoutes);
@@ -44,19 +48,23 @@ mongoose
     .connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-}).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
-    // /*ONLY ADDS DATA TO DB ONCE*/
-    // User.insertMany(dataUser);
-    // // Product.insertMany(dataProduct);
-    //ProductStat.insertMany(dataProductStat);
-    //Transaction.insertMany(dataTransaction);
-//    OverallStat.insertMany(dataOverallStat);
-    //AffiliateStat.insertMany(dataAffiliateStat);
-    
+        // /*ONLY ADDS DATA TO DB ONCE*/
+        // User.insertMany(dataUser);
+        // // Product.insertMany(dataProduct);
+        // ProductStat.insertMany(dataProductStat);
+        // Transaction.insertMany(dataTransaction);
+        // OverallStat.insertMany(dataOverallStat);
+        // AffiliateStat.insertMany(dataAffiliateStat);
+    })
+    .catch((error) => console.log(`{error} did not connect`));
 
+// Add a catch-all route
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'path-to-your-index.html'));
+});
 
-})
-.catch((error) => console.log(`{error} did not connect`) )
-
+export default app;
