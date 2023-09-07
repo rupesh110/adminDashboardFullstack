f805d66a96176fd54754e6e5638d4663ae0b6856
Rupesh <shrestharupesh110@gmail.com>
2023-09-08 00:32:40 +1000

    changed route

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,8 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/" element={<Navigate to="/dashboard"/>} />
+              {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
