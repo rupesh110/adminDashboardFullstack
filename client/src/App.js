771eeef4993eb1e62ae747855d4668e83cdf28f5
Rupesh Shrestha <53545877+rupesh110@users.noreply.github.com>
2024-11-12 13:05:40 +1100

    Update App.js
    Updated fall back url

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,6 @@ function App() {
           <Routes>
             <Route element={<Layout />}>
               <Route path="/" element={<Dashboard/> } />
-              {/* <Route path="/" element={<Navigate to="/dashboard" replace />} /> */}
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
@@ -42,6 +41,7 @@ function App() {
               <Route path="/breakdown" element={<Breakdown />} />
               <Route path="/admin" element={<Admin />} />
               <Route path="/performance" element={<Performance />} />
+               <Route path="/" element={<Navigate to="/dashboard" replace />} />
             </Route>
           </Routes>
         </ThemeProvider>
